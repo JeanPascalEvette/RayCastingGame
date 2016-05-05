d629f3207c9dabfc629f0bf40aa5aad2b1736b90
JeanPascalEvette <jp.evette1@gmail.com>
2016-05-05 15:43:05 +0100

    Basic RayCasting

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,8 +5,11 @@ public class EnemyController : MonoBehaviour {
 
     private GameObject player;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float rotateSpeed = 1f;
+
+    // Use this for initialization
+    void Start () {
         if (player == null)
             player = GameObject.Find("Player");
 
@@ -14,6 +17,7 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        RayCasting.CastRay(transform.position, transform.position + transform.forward * 10.0f, 0.01f);
 	}
 }
---
+++ Assets/Scripts/RayCasting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class RayCasting
+{
+    static GameObject[] defaultColliders = GameObject.FindGameObjectsWithTag("Player");
+
+    public static Vector3[] CastRay(Vector3 startPost, Vector3 endPos, float pollingFrequency, GameObject[] listColliders = null)
+    {
+        if (listColliders == null)
+            listColliders = defaultColliders;
+        List<Vector3> hits = new List<Vector3>();
+        Vector3 direction = (endPos - startPost);
+        float numSteps = direction.magnitude / pollingFrequency;
+        Vector3 previousPos = startPost;
+        Vector3 currentPos = startPost;
+        for (int i = 0; i < (int)(numSteps); i++)
+        {
+            previousPos = currentPos;
+            currentPos += direction * (1.0f/numSteps);
+            if(TestCollision(currentPos, listColliders))
+            {
+                hits.Add(currentPos);
+                Debug.DrawLine(previousPos, currentPos, Color.green);
+            }
+            else
+            {
+                Debug.DrawLine(previousPos, currentPos, Color.red);
+            }
+        }
+
+        return hits.ToArray();
+    }
+
+
+    public static bool TestCollision(Vector3 position, GameObject[] listColliders)
+    {
+        foreach(GameObject collider in listColliders)
+        {
+            var bounds = collider.GetComponent<Collider>().bounds;
+            bool checkX = position.x < bounds.max.x && position.x > bounds.min.x;
+            bool checkY = position.y < bounds.max.y && position.y > bounds.min.y;
+            bool checkZ = position.z < bounds.max.z && position.z > bounds.min.z;
+            if (checkX && checkY && checkZ)
+                return true;
+        }
+        return false;
+    }
+}
+
