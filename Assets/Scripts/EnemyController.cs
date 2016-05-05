59d813b9aaa759c16493a098cca0a80db03ab766
JeanPascalEvette <jp.evette1@gmail.com>
2016-05-05 12:44:55 +0100

    Creating Initial Project

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        if (player == null)
+            player = GameObject.Find("Player");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
