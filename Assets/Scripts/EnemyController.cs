12f77b16ba4138abf6fd5dee4f59d0226f9b69e1
JeanPascalEvette <jp.evette1@gmail.com>
2016-05-07 18:07:49 +0100

    Cleaned up some stuff

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,11 +7,6 @@ public class EnemyController : MonoBehaviour {
 
     [SerializeField]
     private float rotateSpeed = 1f;
-    [SerializeField]
-    private float visionLength = 10.0f;
-    [SerializeField]
-    private float pollingFrequency = 0.01f;
-
 
     private bool playerInSight = false;
 
@@ -31,38 +26,5 @@ public class EnemyController : MonoBehaviour {
         transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
     }
     
-
-    void DrawVision()
-    {
-
-
-        playerInSight = false;
-        CastRay(30);
-        CastRay(25);
-        CastRay(20);
-        CastRay(15);
-        CastRay(10);
-        CastRay(5);
-        CastRay(0);
-        CastRay(5);
-        CastRay(10);
-        CastRay(15);
-        CastRay(20);
-        CastRay(25);
-        CastRay(30);
-        if (playerInSight)
-            GetComponent<Renderer>().sharedMaterial = redMaterial;
-        else
-            GetComponent<Renderer>().sharedMaterial = greenMaterial;
-    }
-
-
-    void CastRay(float rotation)
-    {
-        GameObject visionBreaker = null;
-        RayCasting.CastRay(transform.position, transform.position + Quaternion.Euler(0, rotation, 0) * transform.forward * visionLength, pollingFrequency, out visionBreaker);
-
-        if (visionBreaker != null && visionBreaker.tag == "Player")
-            playerInSight = true;
-    }
+    
 }
--- Assets/Scripts/RayCasting.cs
+++ Assets/Scripts/RayCasting.cs
@@ -66,52 +66,12 @@ public static class RayCasting
 
     }
 
-    public static Vector3[] CastRay(Vector3 startPost, Vector3 endPos, float pollingFrequency, out GameObject visionBreaker, GameObject[] listColliders = null)
-    {
-        visionBreaker = null;
-        if (listColliders == null)
-            listColliders = defaultColliders;
-        List<Vector3> hits = new List<Vector3>();
-        Vector3 direction = (endPos - startPost);
-        float numSteps = direction.magnitude / pollingFrequency;
-        Vector3 previousPos = startPost;
-        Vector3 currentPos = startPost;
-        Transform col;
-        for (int i = 0; i < (int)(numSteps); i++)
-        {
-            previousPos = currentPos;
-            currentPos += direction * (1.0f/numSteps);
-            col = TestCollision(currentPos, listColliders);
-            if (col != null)
-            {
-                hits.Add(currentPos);
-                Debug.DrawLine(previousPos, currentPos, Color.red);
-                var vc = col.GetComponent<VisionCollider>();
-                if (vc != null && !vc.isSeeThrough())
-                {
-                    visionBreaker = col.gameObject;
-                    break;
-                }
-            }
-            else
-            {
-                Debug.DrawLine(previousPos, currentPos, Color.green);
-            }
-        }
-
-        return hits.ToArray();
-    }
-
 
     public static Transform TestCollision(Vector3 position, GameObject[] listColliders)
     {
         foreach(GameObject collider in listColliders)
         {
-            var bounds = collider.GetComponent<Collider>().bounds;
-            bool checkX = position.x < bounds.max.x && position.x > bounds.min.x;
-            bool checkY = position.y < bounds.max.y && position.y > bounds.min.y;
-            bool checkZ = position.z < bounds.max.z && position.z > bounds.min.z;
-            if (checkX && checkY && checkZ)
+            if(collider.GetComponent<Collider>().bounds.Contains(collider.transform.TransformPoint(collider.transform.localRotation * collider.transform.InverseTransformPoint(position))))
                 return collider.transform;
         }
         return null;
